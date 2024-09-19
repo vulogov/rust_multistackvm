4024dcbc87fdd7a4e0f909c9686ba9b5d94df4ea
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-09-19 14:47:49 -0600

    Dev #24

--- src/stdlib/logic/if_fun.rs
+++ src/stdlib/logic/if_fun.rs
@@ -37,8 +37,44 @@ pub fn stdlib_logic_if(vm: &mut VM) -> Result<&mut VM, Error> {
     Ok(vm)
 }
 
+pub fn stdlib_logic_if_stack(vm: &mut VM) -> Result<&mut VM, Error> {
+    if vm.stack.current_stack_len() < 2 {
+        bail!("Stack is too shallow for inline if.stack");
+    }
+    match vm.stack.pull() {
+        Some(lambda_val) => {
+            if lambda_val.is_type(LAMBDA) {
+                match vm.stack.pull() {
+                    Some(condition_val) => {
+                        match condition_val.cast_string() {
+                            Ok(cond) => {
+                                if vm.stack.current_stack_name() == Some(cond) {
+                                    return vm.lambda_eval(lambda_val);
+                                }
+                            }
+                            Err(err) => {
+                                bail!("IF.stack returns error: {}", err);
+                            }
+                        }
+                    }
+                    None => {
+                        bail!("IF.stack returns: NO DATA #2");
+                    }
+                }
+            } else {
+                bail!("IF.stack: #1 parameter must be lambda");
+            }
+        }
+        None => {
+            bail!("IF.stack returns: NO DATA #1");
+        }
+    }
+    Ok(vm)
+}
+
 
 
 pub fn init_stdlib(vm: &mut VM) {
     let _ = vm.register_inline("if".to_string(), stdlib_logic_if);
+    let _ = vm.register_inline("if.stack".to_string(), stdlib_logic_if_stack);
 }
--- tests/vm-logic-if-test.rs
+++ tests/vm-logic-if-test.rs
@@ -33,4 +33,31 @@ mod tests {
         assert_eq!(val.cast_float().unwrap(), 42.0 as f64);
     }
 
+    #[test]
+    fn test_vm_apply_if3() {
+        let mut vm = VM::new();
+        vm.apply(Value::from("main").unwrap()).unwrap();
+        vm.apply(Value::call("lambda".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call(":".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from(42.0).unwrap()).unwrap();
+        vm.apply(Value::call(";".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call("if.stack".to_string(), Vec::new())).unwrap();
+        let val = vm.stack.pull().expect("No pull() happens");
+        assert_eq!(val.cast_float().unwrap(), 42.0 as f64);
+    }
+
+    #[test]
+    fn test_vm_apply_if4() {
+        let mut vm = VM::new();
+        vm.apply(Value::from(42.0).unwrap()).unwrap();
+        vm.apply(Value::from("NO_SUCH_STACK_EXISTS").unwrap()).unwrap();
+        vm.apply(Value::call("lambda".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call(":".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from(43.0).unwrap()).unwrap();
+        vm.apply(Value::call(";".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call("if.stack".to_string(), Vec::new())).unwrap();
+        let val = vm.stack.pull().expect("No pull() happens");
+        assert_eq!(val.cast_float().unwrap(), 42.0 as f64);
+    }
+
 }
