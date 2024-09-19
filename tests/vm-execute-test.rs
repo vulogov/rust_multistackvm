0019b08232145ab19b55fcc842b2057ff7b68709
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-09-19 15:27:31 -0600

    Dev #28

---
+++ tests/vm-execute-test.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+    #![allow(unused_imports)]
+    use super::*;
+    use rust_dynamic::value::Value;
+    use rust_multistack::stack::Stack;
+    use rust_multistackvm::multistackvm::VM;
+
+    #[test]
+    fn test_vm_apply_execute() {
+        let mut vm = VM::new();
+        vm.apply(Value::from(41.0).unwrap()).unwrap();
+        vm.apply(Value::from(42.0).unwrap()).unwrap();
+        vm.apply(Value::ptr(".".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call("execute".to_string(), Vec::new())).unwrap();
+        let val = vm.stack.pull_from_workbench().expect("No pull() happens");
+        assert_eq!(val.cast_float().unwrap(), 42.0 as f64);
+    }
+
+}
