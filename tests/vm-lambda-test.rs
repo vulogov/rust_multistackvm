4d7aab4fc3dc1facc8b64304690a71177960c1b3
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-09-20 11:45:25 -0600

    Dev #39

---
+++ tests/vm-lambda-test.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod tests {
+    #![allow(unused_imports)]
+    use super::*;
+    use rust_dynamic::value::Value;
+    use rust_multistack::stack::Stack;
+    use rust_multistackvm::multistackvm::VM;
+
+    #[test]
+    fn test_vm_apply_lambda_register() {
+        let mut vm = VM::new();
+        // Let's create name for Lambda
+        vm.apply(Value::from("FourtyTwo").unwrap()).unwrap();
+        // Then let's create lambda
+        vm.apply(Value::call("lambda".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call(":".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from(42.0).unwrap()).unwrap();
+        vm.apply(Value::call(";".to_string(), Vec::new())).unwrap();
+        // Call registering and execution
+        vm.apply(Value::call("register".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call("FourtyTwo".to_string(), Vec::new())).unwrap();
+        let val = vm.stack.pull().expect("No pull() happens");
+        assert_eq!(val.cast_float().unwrap(), 42.0 as f64);
+    }
+
+}
