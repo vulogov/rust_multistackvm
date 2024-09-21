8c0a5e9d91b2d0a9bc2f4c765d797f9f6212b5d8
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-09-20 21:37:43 -0600

    Dev #43

--- tests/vm-lambda-test.rs
+++ tests/vm-lambda-test.rs
@@ -23,4 +23,43 @@ mod tests {
         assert_eq!(val.cast_float().unwrap(), 42.0 as f64);
     }
 
+    #[test]
+    fn test_vm_apply_lambda_resolve() {
+        let mut vm = VM::new();
+        // Let's create name for Lambda
+        vm.apply(Value::from("FourtyTwo").unwrap()).unwrap();
+        // Then let's create lambda
+        vm.apply(Value::call("lambda".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call(":".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from(42.0).unwrap()).unwrap();
+        vm.apply(Value::call(";".to_string(), Vec::new())).unwrap();
+        // Call registering and execution
+        vm.apply(Value::call("register".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from("FourtyTwo").unwrap()).unwrap();
+        vm.apply(Value::call("resolve".to_string(), Vec::new())).unwrap();
+        let val = vm.stack.pull().expect("No pull() happens");
+        assert_eq!(val.cast_string().unwrap(), "FourtyTwo");
+    }
+
+    #[test]
+    fn test_vm_apply_alias_resolve() {
+        let mut vm = VM::new();
+        // Let's create name for Lambda
+        vm.apply(Value::from("FourtyTwo").unwrap()).unwrap();
+        // Then let's create lambda
+        vm.apply(Value::call("lambda".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::call(":".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from(42.0).unwrap()).unwrap();
+        vm.apply(Value::call(";".to_string(), Vec::new())).unwrap();
+        // Call registering and execution
+        vm.apply(Value::call("register".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from("FourtyTwo").unwrap()).unwrap();
+        vm.apply(Value::from("answer").unwrap()).unwrap();
+        vm.apply(Value::call("alias".to_string(), Vec::new())).unwrap();
+        vm.apply(Value::from("answer").unwrap()).unwrap();
+        vm.apply(Value::call("resolve".to_string(), Vec::new())).unwrap();
+        let val = vm.stack.pull().unwrap();
+        assert_eq!(val.cast_string().unwrap(), "FourtyTwo");
+    }
+
 }
