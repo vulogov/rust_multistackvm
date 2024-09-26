928cc14874e1782aca34c47a5d3aad588aa6670a
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-09-26 09:38:23 -0600

    Dev #49

--- src/stdlib/print.rs
+++ src/stdlib/print.rs
@@ -45,7 +45,7 @@ pub fn stdlib_nl_inline(vm: &mut VM) -> Result<&mut VM, Error> {
 pub fn init_stdlib(vm: &mut VM) {
     let _ = vm.register_inline("print".to_string(), stdlib_print_inline);
     let _ = vm.register_alias("display".to_string(), "print".to_string());
-    let _ = vm.register_inline("println".to_string(), stdlib_print_inline);
+    let _ = vm.register_inline("println".to_string(), stdlib_println_inline);
     let _ = vm.register_inline("space".to_string(), stdlib_space_inline);
     let _ = vm.register_inline("nl".to_string(), stdlib_nl_inline);
 }
