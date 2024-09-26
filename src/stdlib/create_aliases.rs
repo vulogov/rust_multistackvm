760e6c93132067ca8a15a95cc46d0ea8b997d712
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-09-26 13:41:12 -0600

    Dev #61

--- src/stdlib/create_aliases.rs
+++ src/stdlib/create_aliases.rs
@@ -11,4 +11,8 @@ pub fn init_stdlib(vm: &mut VM) {
     let _ = vm.register_alias(",".to_string(), "concat_with_space".to_string());
     let _ = vm.register_alias("?true".to_string(), "if".to_string());
     let _ = vm.register_alias("?false".to_string(), "if.false".to_string());
+    let _ = vm.register_alias("<-".to_string(), "stacks_left".to_string());
+    let _ = vm.register_alias("->".to_string(), "stacks_right".to_string());
+    let _ = vm.register_alias("<--".to_string(), "rotate_current_left".to_string());
+    let _ = vm.register_alias("-->".to_string(), "rotate_current_right".to_string());
 }
