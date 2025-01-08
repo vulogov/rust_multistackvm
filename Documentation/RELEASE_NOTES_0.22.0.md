56db275a93ee39ed32a91d14711df98c0860d7c7
Vladimir Ulogov <vladimir.ulogov@me.com>
2025-01-08 16:28:20 -0700

    Dev #100

---
+++ Documentation/RELEASE_NOTES_0.22.0.md
@@ -0,0 +1,3 @@
+# What's new in 0.22.0
+
+* Function ```?key``` support checking dict for the key
--- src/stdlib/values/value_dict.rs
+++ src/stdlib/values/value_dict.rs
@@ -66,7 +66,42 @@ pub fn stdlib_value_get(vm: &mut VM) -> Result<&mut VM, Error> {
             }
         }
         None => {
-            bail!("SET returns: NO DATA #1");
+            bail!("GET returns: NO DATA #1");
+        }
+    }
+    Ok(vm)
+}
+
+pub fn stdlib_value_has_key(vm: &mut VM) -> Result<&mut VM, Error> {
+    if vm.stack.current_stack_len() < 2 {
+        bail!("Stack is too shallow for inline get");
+    }
+    match vm.stack.pull() {
+        Some(tag_name_val) => {
+            let key_name = match tag_name_val.cast_string() {
+                Ok(key_name) => key_name,
+                Err(err) => {
+                    bail!("GET key expected to be string: {}", err);
+                }
+            };
+            match vm.stack.pull() {
+                Some(value) => {
+                    match value.has_key(key_name) {
+                        Ok(d_val) => {
+                            vm.stack.push(d_val);
+                        }
+                        Err(err) => {
+                            bail!("HAS_KEY returns error: {}", err);
+                        }
+                    }
+                }
+                None => {
+                    bail!("HAS_KEY returns: NO DATA #2");
+                }
+            }
+        }
+        None => {
+            bail!("HAS_KEY returns: NO DATA #1");
         }
     }
     Ok(vm)
@@ -75,4 +110,5 @@ pub fn stdlib_value_get(vm: &mut VM) -> Result<&mut VM, Error> {
 pub fn init_stdlib(vm: &mut VM) {
     let _ = vm.register_inline("set".to_string(), stdlib_value_set);
     let _ = vm.register_inline("get".to_string(), stdlib_value_get);
+    let _ = vm.register_inline("?key".to_string(), stdlib_value_has_key);
 }
