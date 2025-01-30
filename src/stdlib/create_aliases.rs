d675fd492ab22e4ffc91925c966b7b02b169dc09
Vladimir Ulogov <vladimir.ulogov@me.com>
2025-01-29 23:34:13 -0700

    Dev #116

--- src/stdlib/create_aliases.rs
+++ src/stdlib/create_aliases.rs
@@ -1,7 +1,6 @@
 use crate::multistackvm::VM;
 
 pub fn init_stdlib(vm: &mut VM) {
-    let _ = vm.register_alias("display".to_string(), "print".to_string());
     let _ = vm.register_alias(".".to_string(), "return".to_string());
     let _ = vm.register_alias("!".to_string(), "execute".to_string());
     let _ = vm.register_alias("!.".to_string(), "execute.".to_string());
